3f9692babdf3c3ac35b25afba39ecd1148481eab
apolkingg8 <apolkingg8@gmail.com>
2021-09-02 12:46:50 +0800

    :fire: remove hello world

--- apps/blockchain-mailer/src/main.ts
+++ apps/blockchain-mailer/src/main.ts
@@ -1,1 +1,24 @@
-console.log('Hello World!');
+import BlockchainWatcher from './app/BlockchainWatcher';
+import { sendContractEmail } from './app/Mailer';
+import { emailsFromAddress } from './app/ContractWrap';
+import { ethereumNodeUrl, factoryAddress } from './app/Config';
+
+const newContractWatcher = {
+  address: factoryAddress,
+  topics: [
+    'ChargebackCreated(address)',
+    'PreorderCreated(address)',
+    'EscrowCreated(address)',
+  ],
+  callback: ((log: any, event: any) => {
+    const { email1, email2 } = emailsFromAddress(event.arg1); // TODO
+    sendContractEmail(email1, email2);
+  }),
+};
+
+const blockchainWatch = new BlockchainWatcher({
+  providerUrl: ethereumNodeUrl,
+  watchers: [newContractWatcher],
+})
+
+blockchainWatch.start();
