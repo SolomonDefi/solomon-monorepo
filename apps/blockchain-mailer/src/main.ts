e091327223df729b368d876df511fef776cf019f
apolkingg8 <apolkingg8@gmail.com>
2021-09-02 14:16:48 +0800

    :recycle: refactor Mailer to mailService

--- apps/blockchain-mailer/src/main.ts
+++ apps/blockchain-mailer/src/main.ts
@@ -1,7 +1,7 @@
 import BlockchainWatcher from './app/BlockchainWatcher';
-import { sendContractEmail } from './app/Mailer';
 import { emailsFromAddress } from './app/ContractWrap';
 import envStore from './store/envStore';
+import mailService from './service/mailService';
 
 const newContractWatcher = {
   address: envStore.factoryAddress,
@@ -10,15 +10,15 @@ const newContractWatcher = {
     'PreorderCreated(address)',
     'EscrowCreated(address)',
   ],
-  callback: ((log: any, event: any) => {
+  callback: (async (log: any, event: any) => {
     const { email1, email2 } = emailsFromAddress(event.arg1); // TODO
-    sendContractEmail(email1, email2);
+    await mailService.sendContractEmail(email1, email2)
   }),
-};
+}
 
 const blockchainWatch = new BlockchainWatcher({
   providerUrl: envStore.ethereumNodeUrl,
   watchers: [newContractWatcher],
 })
 
-blockchainWatch.start();
+blockchainWatch.start()
