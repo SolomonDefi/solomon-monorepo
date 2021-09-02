1003bfd9eb28394b7bba829989e9d02fd9c3408c
apolkingg8 <apolkingg8@gmail.com>
2021-09-02 14:16:48 +0800

    :recycle: refactor Mailer to mailService

---
+++ apps/blockchain-mailer/src/service/mailService.ts
@@ -0,0 +1,47 @@
+import MailgunTransport from '../app/util/MailgunTransport';
+import nodemailer, { Transporter } from 'nodemailer';
+
+class MailService {
+
+  mailer: Transporter = null
+
+  send = async (to: string | string[], subject: string, html: string, text: string)=> {
+    this.mailer.sendMail({
+      from: 'solomondefi@gmail.com',
+      to,
+      subject,
+      html,
+      text,
+    }, (err, info) => {
+      if (err) {
+        console.log(`Error: ${err}`)
+      } else {
+        console.log(`Response: ${info}`)
+      }
+    })
+  }
+
+  // Notify both parties to an escrow contract that the contract has been deployed
+  sendContractEmail = async (party1: string, party2: string)=> {
+    // TODO
+    await this.send([party1, party2], 'Escrow contract created', '', '');
+  }
+
+  init = async ()=> {
+    const auth = {
+      auth: {
+        api_key: 'key-1234123412341234',
+        domain: 'one of your domain names listed at your https://app.mailgun.com/app/sending/domains'
+      }
+    }
+    const transport = new MailgunTransport(auth)
+
+    this.mailer = nodemailer.createTransport(transport.getPlugin())
+  }
+
+  constructor() {
+
+  }
+}
+
+export default new MailService()
