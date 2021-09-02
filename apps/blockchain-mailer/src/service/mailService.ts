ca6357bf63ef42728acaf3f7cdf3d996e634fa90
apolkingg8 <apolkingg8@gmail.com>
2021-09-02 14:48:00 +0800

    :recycle: refactor formats

--- apps/blockchain-mailer/src/service/mailService.ts
+++ apps/blockchain-mailer/src/service/mailService.ts
@@ -1,47 +1,43 @@
 import MailgunTransport from '../util/MailgunTransport';
 import nodemailer, { Transporter } from 'nodemailer';
+import envStore from '../store/envStore';
 
 class MailService {
 
-    mailer: Transporter = null;
+    mailer: Transporter = null
 
     constructor() {
 
     }
 
     send = async (to: string | string[], subject: string, html: string, text: string) => {
-        this.mailer.sendMail({
+        let info = await this.mailer.sendMail({
             from: 'solomondefi@gmail.com',
-            to,
-            subject,
-            html,
-            text
-        }, (err, info) => {
-            if (err) {
-                console.log(`Error: ${err}`);
-            } else {
-                console.log(`Response: ${info}`);
-            }
-        });
-    };
+            to: to,
+            subject: subject,
+            html: html,
+            text: text,
+        })
+
+        return info
+    }
 
     // Notify both parties to an escrow contract that the contract has been deployed
     sendContractEmail = async (party1: string, party2: string) => {
         // TODO
-        await this.send([party1, party2], 'Escrow contract created', '', '');
-    };
+        await this.send([party1, party2], 'Escrow contract created', '', '')
+    }
 
     init = async () => {
-        const auth = {
+        const transport = new MailgunTransport({
             auth: {
-                api_key: 'key-1234123412341234',
-                domain: 'one of your domain names listed at your https://app.mailgun.com/app/sending/domains'
+                api_key: envStore.mailgunApiKey,
+                domain: envStore.mailgunDomain,
             }
-        };
-        const transport = new MailgunTransport(auth);
+        })
 
-        this.mailer = nodemailer.createTransport(transport.getPlugin());
-    };
+        this.mailer = nodemailer.createTransport(transport.getPlugin())
+    }
 }
 
-export default new MailService();
+export default new MailService()
