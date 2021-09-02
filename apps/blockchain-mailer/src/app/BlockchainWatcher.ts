15ca6e608d485fa3e9733f46f826ccf47c8c9efc
apolkingg8 <apolkingg8@gmail.com>
2021-09-02 14:24:32 +0800

    :wrench: change indent size from 2 to 4

--- apps/blockchain-mailer/src/app/BlockchainWatcher.ts
+++ apps/blockchain-mailer/src/app/BlockchainWatcher.ts
@@ -3,37 +3,37 @@ import { ethers } from 'ethers';
 // TODO -- import contract ABIs
 
 export default class BlockchainWatcher {
-  watchers: any[];
-  provider: any;
+    watchers: any[];
+    provider: any;
 
-  constructor(options: any) {
-    this.watchers = options.watchers;
-    this.provider = new ethers.providers.JsonRpcProvider(options.providerUrl);
-  }
-
-  start() {
-    if(this.watchers.length === 0) {
-      throw new Error('Abort! At least one watcher must be set up');
+    constructor(options: any) {
+        this.watchers = options.watchers;
+        this.provider = new ethers.providers.JsonRpcProvider(options.providerUrl);
     }
-    for(let watcher of this.watchers) {
-      const filter = {
-        address: watcher.address,
-        topics: (watcher.topics || []).map((t: string) => ethers.utils.id(t)),
-      }
-      // TODO parse/deserialize results
-      this.provider.on(filter, watcher.callback);
+
+    start() {
+        if (this.watchers.length === 0) {
+            throw new Error('Abort! At least one watcher must be set up');
+        }
+        for (let watcher of this.watchers) {
+            const filter = {
+                address: watcher.address,
+                topics: (watcher.topics || []).map((t: string) => ethers.utils.id(t))
+            };
+            // TODO parse/deserialize results
+            this.provider.on(filter, watcher.callback);
+        }
     }
-  }
 
-  removeWatcher(name: string) {
-    this.watchers = this.watchers.filter(w => w.name !== name);
-  }
+    removeWatcher(name: string) {
+        this.watchers = this.watchers.filter(w => w.name !== name);
+    }
 
-  addWatcher(watcher: any) {
-    this.watchers.push(watcher);
-  }
+    addWatcher(watcher: any) {
+        this.watchers.push(watcher);
+    }
 
-  setWatchers(watchers: any[]) {
-    this.watchers = watchers;
-  }
+    setWatchers(watchers: any[]) {
+        this.watchers = watchers;
+    }
 }
--- apps/blockchain-mailer/src/store/envStore.ts
+++ apps/blockchain-mailer/src/store/envStore.ts
@@ -1,18 +1,17 @@
-
 class EnvStore {
 
-  get isDev() {
-    return process.env.NODE_ENV === "development"
-  }
+    get isDev() {
+        return process.env.NODE_ENV === 'development';
+    }
 
-  get ethereumNodeUrl() {
-    return process.env.ETHEREUM_NODE_URL
-  }
+    get ethereumNodeUrl() {
+        return process.env.ETHEREUM_NODE_URL;
+    }
 
-  get factoryAddress() {
-    return process.env.FACTORY_ADDRESS
-  }
+    get factoryAddress() {
+        return process.env.FACTORY_ADDRESS;
+    }
 
 }
 
-export default new EnvStore()
+export default new EnvStore();
