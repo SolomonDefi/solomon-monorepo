2ba4118e5771aa60e0d81eab8e98c5698275e839
apolkingg8 <apolkingg8@gmail.com>
2021-09-06 15:47:43 +0800

    :construction_worker: [root]: add auto release to CI #8

---
+++ release.config.js
@@ -0,0 +1,44 @@
+const { promisify } = require('util')
+const dateFormat = require('dateformat')
+const path = require('path')
+const readFileAsync = promisify(require('fs').readFile)
+
+// Given a `const` variable `TEMPLATE_DIR` which points to "<semantic-release-gitmoji>/lib/assets/templates"
+const TEMPLATE_DIR = `node_modules/semantic-release-gitmoji/lib/assets/templates`
+// the *.hbs template and partials should be passed as strings of contents
+const template = readFileAsync(path.join(TEMPLATE_DIR, 'default-template.hbs'))
+const commitTemplate = readFileAsync(path.join(TEMPLATE_DIR, 'commit-template.hbs'))
+
+module.exports = {
+    branches: ["master"],
+    plugins: [
+        [
+            'semantic-release-gitmoji', {
+            releaseRules: {
+                major: [ ':boom:' ],
+                minor: [ ':sparkles:' ],
+                patch: [
+                    ':bug:',
+                    ':ambulance:',
+                    ':lock:'
+                ]
+            },
+            releaseNotes: {
+                template,
+                partials: { commitTemplate },
+                helpers: {
+                    datetime: function (format = 'UTC:yyyy-mm-dd') {
+                        return dateFormat(new Date(), format)
+                    }
+                },
+                issueResolution: {
+                    template: '{baseUrl}/{owner}/{repo}/issues/{ref}',
+                    baseUrl: 'https://github.com',
+                    source: 'github.com'
+                }
+            }
+        }
+        ],
+        '@semantic-release/github',
+    ]
+}
