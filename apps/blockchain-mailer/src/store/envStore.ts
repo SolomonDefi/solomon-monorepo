170a07502265ad59c64c2b0b84cb0f2b3f98be72
apolkingg8 <apolkingg8@gmail.com>
2021-09-06 16:06:41 +0800

    :bug: [blockchain-mailer]: fix type error #7

--- apps/blockchain-mailer/src/store/envStore.ts
+++ apps/blockchain-mailer/src/store/envStore.ts
@@ -1,26 +1,26 @@
 class EnvStore {
-  get isDev() {
+  get isDev(): boolean {
     return process.env.NODE_ENV === 'development'
   }
 
-  get isTest() {
+  get isTest(): boolean {
     return process.env.NODE_ENV === 'test'
   }
 
-  get ethereumNodeUrl() {
-    return process.env.ETHEREUM_NODE_URL
+  get ethereumNodeUrl(): string {
+    return process.env.ETHEREUM_NODE_URL || ''
   }
 
-  get factoryAddress() {
-    return process.env.FACTORY_ADDRESS
+  get factoryAddress(): string {
+    return process.env.FACTORY_ADDRESS || ''
   }
 
-  get mailgunApiKey() {
+  get mailgunApiKey(): string {
     return this.isDev ? 'key-1234123412341234' : ''
   }
 
   // one of your domain names listed at your https://app.mailgun.com/app/sending/domains
-  get mailgunDomain() {
+  get mailgunDomain(): string {
     return ''
   }
 }
