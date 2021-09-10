c26701f9f74f01816687b646e2ebba091b96610b
apolkingg8 <apolkingg8@gmail.com>
2021-09-10 17:18:49 +0800

    :recycle: [api]: refactor watcher pattern #15

--- apps/blockchain-mailer/src/main.ts
+++ apps/blockchain-mailer/src/main.ts
@@ -1,30 +1,9 @@
-import envStore from './store/envStore';
 import mailService from './service/mailService';
-import ContractWatcher from './klass/ContractWatcher';
-import watcherService from './service/watcherService';
+import ethService from "./service/ethService";
 
 let start = async () => {
   await mailService.init()
-
-  const newContractWatcher = new ContractWatcher({
-    address: envStore.factoryAddress,
-    topics: [
-      'ChargebackCreated(address)',
-      'PreorderCreated(address)',
-      'EscrowCreated(address)',
-    ],
-    callback: async (log: any, event: any) => {
-      // TODO -- TESTING ONLY, REPLACE WITH REAL METHOD
-      const email1 = 'solomondefi@gmail.com'
-      const email2 = 'solomondefi@gmail.com'
-
-      await mailService.sendContractEmail(email1, email2)
-    },
-  })
-
-  watcherService.addWatcher(newContractWatcher)
-  watcherService.setProvider(envStore.ethereumNodeUrl)
-  watcherService.start()
+  await ethService.init()
 }
 
 start()
