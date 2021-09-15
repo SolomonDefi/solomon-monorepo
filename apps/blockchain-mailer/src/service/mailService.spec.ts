14e30906d993f56d53c631a8d992e799856fb239
apolkingg8 <apolkingg8@gmail.com>
2021-09-15 18:31:31 +0800

    :white_check_mark: [api]: init test for mailService #17

--- apps/blockchain-mailer/src/service/mailService.spec.ts
+++ apps/blockchain-mailer/src/service/mailService.spec.ts
@@ -1,7 +1,22 @@
 import mailService from "./mailService";
+import {exec} from "child_process";
+import path from "path";
+import pathStore from "../../../../pathStore";
+import {promisify} from "util";
+import {existsSync} from "fs";
 
 describe('mailService', () => {
   test('constructor()', async () => {
     expect(mailService).toBeDefined()
+  })
+
+  test("getTemplateHtml()", async ()=> {
+    let scriptPath = path.resolve(pathStore.scripts, 'generateMailTemplates.ts')
+
+    await promisify(exec)(`pnpx ts-node ${scriptPath}`)
+
+    let htmlPath = path.resolve(pathStore.mailer, 'src', 'template', '_test.html')
+
+    expect(existsSync(htmlPath)).toEqual(true)
   })
 })
