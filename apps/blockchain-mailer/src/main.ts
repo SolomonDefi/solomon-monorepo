b49ff28f7b16a46843ca28bc69d842d8300fdee7
apolkingg8 <apolkingg8@gmail.com>
2021-09-22 15:54:12 +0800

    :construction: [api]: Use a mock init for mailer #19

--- apps/blockchain-mailer/src/main.ts
+++ apps/blockchain-mailer/src/main.ts
@@ -1,12 +1,10 @@
 import 'reflect-metadata'
-import mailService from './service/mailService'
-import ethService from './service/ethService'
-import dbService from './service/dbService'
 
 let start = async () => {
-  await dbService.init()
-  await mailService.init()
-  await ethService.init()
+  // await dbService.init()
+  // await mailService.init()
+  // await ethService.init()
+  console.log(`Blockchain mailer server start success.`)
 }
 
 start()
