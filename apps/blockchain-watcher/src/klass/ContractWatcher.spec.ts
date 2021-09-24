be1749c25842cd11f4c35fa490e27e6197a8e496
apolkingg8 <apolkingg8@gmail.com>
2021-09-24 13:51:51 +0800

    :fire: [api]: remove unused files #75

--- apps/blockchain-watcher/src/klass/ContractWatcher.spec.ts
+++
@@ -1,9 +0,0 @@
-import ContractWatcher from './ContractWatcher'
-
-describe('ContractWatcher', () => {
-  test('constructor()', async () => {
-    let watcher = new ContractWatcher({})
-
-    expect(watcher).toBeInstanceOf(ContractWatcher)
-  })
-})
