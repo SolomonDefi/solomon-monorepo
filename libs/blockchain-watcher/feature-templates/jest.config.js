d1578d162db45d856ba739b84c524068c2d40daf
apolkingg8 <apolkingg8@gmail.com>
2021-09-24 15:07:03 +0800

    :white_check_mark: [api]: fix jest test #69

--- libs/blockchain-watcher/feature-templates/jest.config.js
+++ libs/blockchain-watcher/feature-templates/jest.config.js
@@ -11,5 +11,5 @@ module.exports = {
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../../coverage/libs/blockchain-mailer/feature-templates',
+  coverageDirectory: '../../../coverage/libs/blockchain-watcher/feature-templates',
 }
