3c2aa092a77c64cca7fb5644e121f4b19c1737aa
Kelvin <kelvin60429@gmail.com>
2021-09-30 16:31:48 +0800

    :sparkles: [api]: add dispute API backend app #61

---
+++ apps/api-dispute/src/app/api/__init__.py
@@ -0,0 +1,9 @@
+from fastapi import APIRouter
+from fastapi.responses import JSONResponse
+
+api_router = APIRouter()
+
+
+@api_router.get('/health/app')
+def healthcheck() -> None:
+    return JSONResponse({'status': 'ok'})
