3270a3e12b8caf8da2f1e732f7e68759d1233fb5
SolomonDefi <solomondefi@gmail.com>
2021-10-12 12:55:09 +0800

    :green_heart: [root]: add initial k8s config #2

--- apps/db/src/app/scripts/init-db.sh
+++ apps/db/src/app/scripts/init-db.sh
@@ -3,18 +3,18 @@
 # Print each command and exit on error
 # set -x
 
-DB_SERVICE_HOST=${DB_API_SERVICE_HOST:=localhost}
-DB_SERVICE_PORT=${DB_API_SERVICE_PORT:=5432}
-DB_USER=${DB_API_USER:=postgres}
+DB_SERVICE_HOST=${DB_SERVICE_HOST:=localhost}
+DB_SERVICE_PORT=${DB_SERVICE_PORT:=5432}
+DB_USER=${DB_USER:=postgres}
 DB_CONNECT_OPTS="-h $DB_SERVICE_HOST -p $DB_SERVICE_PORT -U $DB_USER -d postgres"
 DB_PASSWORD_FILE=~/.pgpass
-DB_PASSWORD_FILE_ENTRY=$DB_SERVICE_HOST:$DB_SERVICE_PORT:*:$DB_USER:$DB_API_PASSWORD
+DB_PASSWORD_FILE_ENTRY=$DB_SERVICE_HOST:$DB_SERVICE_PORT:*:$DB_USER:$DB_PASSWORD
 knex_exec=node_modules/.bin/knex
 knex_dir=apps/db-api/src/app
 
 # echo "[DEBUG] NODE_ENV=$NODE_ENV"
 # echo "[DEBUG] DB_SERVICE_HOST=$DB_SERVICE_HOST"
-# echo "[DEBUG] DB_USER=$DB_API_USER"
+# echo "[DEBUG] DB_USER=$DB_USER"
 # echo "[DEBUG] DB_CONNECT_OPTS=$DB_CONNECT_OPTS"
 
 echo "[INFO] Create PG password file if does not exist..."
@@ -24,22 +24,22 @@ grep -q -F $DB_PASSWORD_FILE_ENTRY $DB_PASSWORD_FILE || echo $DB_PASSWORD_FILE_E
 
 db_exists=true
 echo "[INFO] Check if app DB exists..."
-psql $DB_CONNECT_OPTS -tc "SELECT 1 FROM pg_database WHERE datname = '$DB_API_NAME'" | \
+psql $DB_CONNECT_OPTS -tc "SELECT 1 FROM pg_database WHERE datname = '$DB_NAME'" | \
   grep -q 1 || db_exists=false
 
 if [[ $NODE_ENV = "dev" && "$db_exists" = true ]]; then
   echo "[INFO] Drop app and test databases in dev environment..."
   # Option #1 (Dropping app database)
-  psql $DB_CONNECT_OPTS -c "UPDATE pg_database SET datallowconn = 'false' WHERE datname = '$DB_API_NAME'"
-  psql $DB_CONNECT_OPTS -c "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = '$DB_API_NAME'"
-  psql $DB_CONNECT_OPTS -c "DROP DATABASE $DB_API_NAME"
-  # psql $DB_CONNECT_OPTS -c "DROP DATABASE ${DB_API_NAME}_test"
+  psql $DB_CONNECT_OPTS -c "UPDATE pg_database SET datallowconn = 'false' WHERE datname = '$DB_NAME'"
+  psql $DB_CONNECT_OPTS -c "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = '$DB_NAME'"
+  psql $DB_CONNECT_OPTS -c "DROP DATABASE $DB_NAME"
+  # psql $DB_CONNECT_OPTS -c "DROP DATABASE ${DB_NAME}_test"
   db_exists=false
 fi
 
 if [[ $db_exists != true ]]; then
   echo "[INFO] Create app database..."
-  psql $DB_CONNECT_OPTS -c "CREATE DATABASE $DB_API_NAME"
+  psql $DB_CONNECT_OPTS -c "CREATE DATABASE $DB_NAME"
 fi
 
 # TODO - commented out knex migrations, we may use something else
@@ -53,8 +53,8 @@ fi
 #   echo "[INFO] Seeding dev database..."
 #   NODE_ENV=$NODE_ENV $knex_exec --knexfile $knex_dir/knexfile.js seed:run
   # echo "[INFO] Create test database..."
-  # psql $DB_CONNECT_OPTS -tc "SELECT 1 FROM pg_database WHERE datname = '${DB_API_NAME}_test'" | \
-  #   grep -q 1 || psql $DB_CONNECT_OPTS -c "CREATE DATABASE ${DB_API_NAME}_test"
+  # psql $DB_CONNECT_OPTS -tc "SELECT 1 FROM pg_database WHERE datname = '${DB_NAME}_test'" | \
+  #   grep -q 1 || psql $DB_CONNECT_OPTS -c "CREATE DATABASE ${DB_NAME}_test"
   # echo "[INFO] Migrate test database..."
   # NODE_ENV=test $knex_exec --knexfile $knex_dir/knexfile.js migrate:latest
 # fi
--- apps/db/src/app/scripts/wait-db.sh
+++ apps/db/src/app/scripts/wait-db.sh
@@ -3,9 +3,9 @@
 # Print each command and exit on error
 # set -x
 
-DB_SERVICE_HOST=${DB_API_SERVICE_HOST:=localhost}
-DB_SERVICE_PORT=${DB_API_SERVICE_PORT:=5432}
-DB_USER=${DB_API_USER:=postgres}
+DB_SERVICE_HOST=${DB_SERVICE_HOST:=localhost}
+DB_SERVICE_PORT=${DB_SERVICE_PORT:=5432}
+DB_USER=${DB_USER:=postgres}
 DB_CONNECT_OPTS="-h $DB_SERVICE_HOST -p $DB_SERVICE_PORT -U $DB_USER"
 
 # Wait for the DB service to be up
