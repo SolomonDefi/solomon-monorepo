cc67bc3bb32693c4a0ac33bfe5d3a14df229a0de
apolkingg8 <apolkingg8@gmail.com>
2021-10-18 18:38:57 +0800

    :construction: [root]: init blockchain-watcher-e2e #32

---
+++ apps/blockchain-watcher-e2e/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  displayName: 'blockchain-watcher-e2e',
+  preset: '../../jest.preset.ts',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/blockchain-watcher-e2e',
+}
