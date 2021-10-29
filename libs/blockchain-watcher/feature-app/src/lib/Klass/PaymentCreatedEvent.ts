98698d52e0436c23862d4711e02de8ff4f58a218
apolkingg8 <apolkingg8@gmail.com>
2021-10-29 16:45:29 +0800

    :art: [api]: add class validation #93

--- libs/blockchain-watcher/feature-app/src/lib/Klass/PaymentCreatedEvent.ts
+++ libs/blockchain-watcher/feature-app/src/lib/Klass/PaymentCreatedEvent.ts
@@ -1,26 +1,47 @@
 import { EnumPaymentCreatedEventType } from '../Enum/EnumPaymentCreatedEventType'
 import { v4 } from 'uuid'
+import { IsEnum, IsNotIn, IsNumber, IsString, IsUUID, Max, Min } from 'class-validator'
+import { IsEthAddress } from '../util/IsEthAddress'
 
 export class PaymentCreatedEvent {
+  @IsUUID()
+  @IsString()
   id: string = v4()
+
+  @IsNotIn([EnumPaymentCreatedEventType.unknown])
+  @IsEnum(EnumPaymentCreatedEventType)
   type: EnumPaymentCreatedEventType = EnumPaymentCreatedEventType.unknown
+
+  @IsEthAddress()
+  @IsString()
   party1: string = ''
+
+  @IsEthAddress()
+  @IsString()
   party2: string = ''
+
+  @IsEthAddress()
+  @IsString()
   contract: string = ''
+
+  @IsEthAddress()
+  @IsString()
   judgeContract: string = ''
+
+  @IsEthAddress()
+  @IsString()
   token: string = ''
+
+  @Min(0)
+  @Max(100)
+  @IsNumber()
   discount: number = 0
+
+  @Min(0)
+  @IsNumber()
   ethPaid: number = 0
 
   constructor(props: Partial<PaymentCreatedEvent>) {
     Object.assign(this, props)
-
-    if (this.type === EnumPaymentCreatedEventType.unknown) {
-      throw 'type can not be unknown'
-    }
-
-    if (this.discount < 0 || this.discount > 100) {
-      throw 'discount must between 0-100'
-    }
   }
 }
