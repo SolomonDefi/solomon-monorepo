5526836889a5f307597239cb943127044083e9e4
apolkingg8 <apolkingg8@gmail.com>
2021-11-02 14:36:12 +0800

    :bug: [api]: add init functions #93

--- apps/blockchain-watcher/src/main.ts
+++ apps/blockchain-watcher/src/main.ts
@@ -1,9 +1,19 @@
-import { dbService, mailService } from '@solomon/blockchain-watcher/feature-app'
+import {
+  appService,
+  dbService,
+  deliverService,
+  ethService,
+  mailService,
+  webhookService,
+} from '@solomon/blockchain-watcher/feature-app'
 
 let start = async () => {
   await dbService.init()
+  await ethService.init()
   await mailService.init()
-  // await ethService.init()
+  await deliverService.init()
+  await webhookService.init()
+  await appService.init()
   console.log(`Blockchain watcher server start success.`)
 }
 
