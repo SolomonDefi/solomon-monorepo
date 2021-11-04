65b3dcfd43a5e27782c92d2c52f537a1f29bc13e
apolkingg8 <apolkingg8@gmail.com>
2021-11-04 16:01:45 +0800

    :construction: [root]: change /health-check to GET only #93

--- apps/blockchain-watcher-e2e/src/api.spec.ts
+++ apps/blockchain-watcher-e2e/src/api.spec.ts
@@ -13,24 +13,10 @@ describe('Watcher api services', () => {
   })
 
   it('/health-check', async () => {
-    let getRes = await fetch(`http://localhost:3001/health-check`, { method: 'get' })
-    let postRes = await fetch(`http://localhost:3001/health-check`, { method: 'post' })
-    let putRes = await fetch(`http://localhost:3001/health-check`, { method: 'put' })
-    let deleteRes = await fetch(`http://localhost:3001/health-check`, {
-      method: 'delete',
-    })
+    let res = await fetch(`http://localhost:3001/health-check`, { method: 'get' })
 
-    expect(getRes.ok).toEqual(true)
-    expect(getRes.status).toEqual(204)
-    expect(await getRes.text()).toEqual('')
-    expect(postRes.ok).toEqual(true)
-    expect(postRes.status).toEqual(204)
-    expect(await postRes.text()).toEqual('')
-    expect(putRes.ok).toEqual(true)
-    expect(putRes.status).toEqual(204)
-    expect(await putRes.text()).toEqual('')
-    expect(deleteRes.ok).toEqual(true)
-    expect(deleteRes.status).toEqual(204)
-    expect(await deleteRes.text()).toEqual('')
+    expect(res.ok).toEqual(true)
+    expect(res.status).toEqual(204)
+    expect(await res.text()).toEqual('')
   })
 })
