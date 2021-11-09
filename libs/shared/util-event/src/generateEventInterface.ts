014624159fc498e0e4a88110ed45705c17dee6e7
apolkingg8 <apolkingg8@gmail.com>
2021-11-09 15:31:00 +0800

    :sparkles: [api]: generate script #146

--- libs/shared/util-event/src/generateEventInterface.ts
+++ libs/shared/util-event/src/generateEventInterface.ts
@@ -1,8 +1,34 @@
 import { compile, compileFromFile } from 'json-schema-to-typescript'
-import { write, writeFile } from "fs-extra";
+import { readFile, write, writeFile } from "fs-extra";
 import path from "path";
+import { readdir } from "fs-extra";
 
 export const generateEventInterface = async ()=> {
-  let res = await compileFromFile(path.resolve(__dirname, '..', '_sample.schema.json'))
-  await writeFile(path.resolve(__dirname, 'test.schema.ts'), res, 'utf-8')
+  const interfaceFolderPath = path.resolve(__dirname, '..', 'interface')
+  const schemaFolderPath = path.resolve(__dirname, '..', 'schema')
+  const schemaFileNames = await readdir(schemaFolderPath)
+  let indexStr = ``
+
+  for(let schemaFileName of schemaFileNames) {
+    try {
+      const schemaFilePath = path.resolve(schemaFolderPath, schemaFileName)
+      const interfaceFileName = schemaFileName.replace('.schema.json', '.schema.ts')
+      const interfaceFilePath = path.resolve(interfaceFolderPath, interfaceFileName)
+
+      let interfaceStr = await compileFromFile(schemaFilePath, {
+        unreachableDefinitions: true,
+      })
+
+      interfaceStr = interfaceStr.replace(/\[k: string]: unknown;/g, ``)
+
+      await writeFile(interfaceFilePath, interfaceStr, 'utf-8')
+      indexStr += `export * from '../interface/${interfaceFileName.replace('.ts', '')}'\n`
+      console.log(`${interfaceFilePath} generated.`)
+    } catch (err) {
+      console.error(err)
+      console.log(`${schemaFileName} generate fail.`)
+    }
+  }
+
+  await writeFile(path.resolve(__dirname, 'index.ts'), indexStr)
 }
