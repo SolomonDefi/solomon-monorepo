4dc9a61de6b6270fcdb6ea7691c1fa6654b3fada
Jonathan <jonathant888@gmail.com>
2021-11-09 11:39:45 +0800

    :sparkle: [blockchain]: Updated tiebreaker functionality and included timeout period #143

--- apps/contracts/src/contracts/library/SlmJudgement.sol
+++ apps/contracts/src/contracts/library/SlmJudgement.sol
@@ -21,6 +21,10 @@ contract SlmJudgement is Ownable {
 
     uint256[] public stakerPool;
 
+    uint256 public tieBreakerDuration;
+
+    mapping(address => uint256) public tieBreakerEndTimes;
+
     modifier onlyOwnerOrManager() {
         require(msg.sender == owner || msg.sender == address(stakerManager), "Unauthorized access");
         _;
@@ -52,11 +56,13 @@ contract SlmJudgement is Ownable {
     /// @dev Mapping of dispute address to latest index for Round Robin Mapping
     mapping(address => uint32) public jurorSelectionIndex;
 
-    constructor(address newStakerManager, uint16 newMinJurorCount) {
+    constructor(address newStakerManager, uint16 newMinJurorCount, uint256 newTieBreakerDuration) {
         require(newStakerManager != address(0), "Zero addr");
-        require(newMinJurorCount > 3, "Invalid juror count");
+        require(newMinJurorCount > 0, "Invalid juror count");
+        require(newTieBreakerDuration > 0, "Invalid duration");
         stakerManager = SlmStakerManager(newStakerManager);
         minJurorCount = newMinJurorCount;
+        tieBreakerDuration = newTieBreakerDuration;
     }
 
     function setStakerManager(address newStakerManager) external onlyOwner {
@@ -65,7 +71,7 @@ contract SlmJudgement is Ownable {
     }
 
     function setMinJurorCount(uint16 newMinJurorCount) external onlyOwner {
-        require(newMinJurorCount >= 3, "Invalid juror count");
+        require(newMinJurorCount >= 0, "Invalid juror count");
         minJurorCount = newMinJurorCount;
     }
 
@@ -74,7 +80,7 @@ contract SlmJudgement is Ownable {
         require(quorum > 0, "Invalid number");
         require(endTime > 0, "Invalid number");
         // TODO -- Access control
-        this.setJurors(slmContract);
+        _setJurors(slmContract);
         disputes[slmContract].quorum = quorum;
         stakerManager.setVoteDetails(slmContract, endTime);
     }
@@ -94,81 +100,96 @@ contract SlmJudgement is Ownable {
     function setAdminRights(address walletAddress) external onlyOwner{
         require(walletAddress != address(0), "Zero addr");
 
-        adminList(walletAddress) = true;
+        adminList[walletAddress] = true;
     }
 
     function removeAdminRights(address walletAddress) external onlyOwner{
         require(walletAddress != address(0), "Zero addr");
-        require(adminList(walletAddress) == true, "Not an admin");
+        require(this.adminList(walletAddress) == true, "Not an admin");
 
-        adminList(walletAddress) = false;
+        adminList[walletAddress] = false;
     }
 
-    function getAdminRights(address walletAddress) external returns(bool) {
+    function getAdminRights(address walletAddress) external view returns(bool) {
         require(walletAddress != address(0), "Zero addr");
-        if(adminList(walletAddress) == true) {
+        if(this.adminList(walletAddress) == true) {
             return true;
         }
         return false;
     }
 
+    function setTieBreakerDuration(uint256 newTieBreakerDuration) external onlyOwner{
+        require(tieBreakerDuration > 0, "Invalid duration");
+        tieBreakerDuration = newTieBreakerDuration;
+    }
+
+    function _startTieBreaker(address slmContract) private {
+        require(slmContract != address(0), "Zero addr");
+        tieBreakerEndTimes[slmContract] = block.timestamp + tieBreakerDuration;
+    }
+
     function tieBreaker(address slmContract, bool buyerWins) external {
         require(slmContract != address(0), "Zero addr");
-        require(adminList(msg.sender) == true, "Not an admin");
-        require(voteResults(slmContract) == 4, "Not a tie");
-
-        if(buyerWins) {
-            voteResults(slmContract) = 2;
+        if(tieBreakerEndTimes[slmContract] < block.timestamp) {
+            voteResults[slmContract] = 2;
         } else {
-            voteResults(slmContract) = 3;
-        }
-        
+            require(tieBreakerEndTimes[slmContract] > block.timestamp, "Tie breaker has ended");
+            require(adminList[msg.sender] == true, "Not an admin");
+            require(voteResults[slmContract] == 4, "Not a tie");
+
+            if(buyerWins) {
+                voteResults[slmContract] = 2;
+            } else {
+                voteResults[slmContract] = 3;
+            }
+        }        
     }
 
     /// Get the result of a contract dispute
     /// @param slmContract Contract to check dispute status
-    function voteStatus(address slmContract) public view returns(uint8) {
+    function voteStatus(address slmContract) public {
         require(slmContract != address(0), "Zero addr");
         Dispute storage dispute = disputes[slmContract];
         uint16 merchantVotes = dispute.merchantVoteCount;
         uint16 buyerVotes = dispute.buyerVoteCount;
         // No vote exists
         if(dispute.quorum == 0) {
-            voteResults(slmContract) = 0;
+            voteResults[slmContract] = 0;
         }
         // Vote not complete
         else if(merchantVotes + buyerVotes < dispute.quorum) {
-            voteResults(slmContract) = 1;
+            voteResults[slmContract] = 1;
         }
         else if(buyerVotes > merchantVotes) {
-            voteResults(slmContract) = 2;
+            voteResults[slmContract] = 2;
         }
         else if(merchantVotes > buyerVotes) {
-            voteResults(slmContract) = 3;
+            voteResults[slmContract] = 3;
         }
         // Tie breaker
         else {
-            voteResults(slmContract) = 4;
+            voteResults[slmContract] = 4;
+            _startTieBreaker(slmContract);
         }
     }
 
-    function getVoteResults(address slmContract) external returns(uint8) {
+    function getVoteResults(address slmContract) external view returns(uint8) {
         require(slmContract != address(0), "Zero addr");
 
-        return voteResults(slmContract);
+        return voteResults[slmContract];
     }
 
     function setStakerPool() external onlyOwner {
         stakerPool = stakerManager.getStakerPool();
     }
 
-    function setJurors(address slmContract) external onlyOwner {
+    function _setJurors(address slmContract) private {
         require(slmContract != address(0), "Zero addr");
         uint256[] memory selectedJurors = _selectJurorList(slmContract);
         jurorList[slmContract] = selectedJurors;
     }
 
-    function getJurors(address slmContract) external view onlyOwnerOrManager returns(uint256[] memory) {
+    function getJurors(address slmContract) external onlyOwnerOrManager view returns(uint256[] memory) {
         require(slmContract != address(0), "Zero addr");
         return jurorList[slmContract];
     }
