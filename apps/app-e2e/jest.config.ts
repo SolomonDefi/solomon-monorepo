fc602bbd3491d6f179af948adb7929e11a65e406
apolkingg8 <apolkingg8@gmail.com>
2021-11-26 17:41:42 +0800

    :construction: [api]: init app e2e #161

---
+++ apps/app-e2e/jest.config.ts
@@ -0,0 +1,15 @@
+module.exports = {
+  displayName: 'app-e2e',
+  preset: '../../jest.preset.ts',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/app-e2e',
+}
