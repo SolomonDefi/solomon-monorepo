5b466486c1764ceeae7cbcd54b2e941b6da52647
Jonathan <jonathant888@gmail.com>
2021-12-03 16:16:23 +0800

    :sparkles: [blockchain]: update tie handling #164

--- apps/contracts/src/contracts/SlmPreorder.sol
+++ apps/contracts/src/contracts/SlmPreorder.sol
@@ -9,6 +9,12 @@ import "./library/SlmJudgement.sol";
 /// @notice A contract that holds ETH or ERC20 tokens for preorders/crowdfunding until release conditions are met
 contract SlmPreorder is SlmShared {
 
+    bool finalWithdrawal = false;
+
+    bool merchantWithdrawalComplete = false;
+
+    bool buyerWithdrawalComplete = false;
+
     /// Initialize the contract
     /// @param _judge Contract that assigns votes for refund requests
     /// @param _token Token for ERC20 payments
@@ -70,18 +76,56 @@ contract SlmPreorder is SlmShared {
     function buyerWithdraw(bytes32 encryptionKey) external {
         require(msg.sender == buyer(), "Only buyer can withdraw");
         judge.authorizeUser(address(this), msg.sender, encryptionKey);
-        require(judge.getVoteResults(address(this), encryptionKey) == SlmJudgement.VoteStates.BuyerWins, "Cannot withdraw");
-        state = TransactionState.CompleteParty1;
-        withdraw(buyer(), owner);
+
+        bool eligibleWithdrawal = false;
+        bool isTie = false;
+        SlmJudgement.VoteStates voteResult = judge.getVoteResults(address(this), encryptionKey);
+        if (voteResult == SlmJudgement.VoteStates.BuyerWins) {
+            eligibleWithdrawal = true;
+        } else if (voteResult == SlmJudgement.VoteStates.Tie) {
+            isTie = true;
+        }
+        require(eligibleWithdrawal || isTie, "Cannot withdraw");
+
+        if (finalWithdrawal) {
+            state = TransactionState.CompleteTie;
+        } else {
+            state = TransactionState.CompleteParty1;
+        }
+
+        if (!buyerWithdrawalComplete) {
+            withdraw(buyer(), owner, isTie, finalWithdrawal);
+        }
+        buyerWithdrawalComplete = true;
+        finalWithdrawal = true;
     }
 
     /// Allow merchant to withdraw if eligible
     function merchantWithdraw(bytes32 encryptionKey) external {
         require(msg.sender == merchant(), "Only merchant can withdraw");
         judge.authorizeUser(address(this), msg.sender, encryptionKey);
-        require(judge.getVoteResults(address(this), encryptionKey) == SlmJudgement.VoteStates.MerchantWins, "Cannot withdraw");
-        state = TransactionState.CompleteParty2;
-        withdraw(merchant(), owner);
+
+        bool eligibleWithdrawal = false;
+        bool isTie = false;
+        SlmJudgement.VoteStates voteResult = judge.getVoteResults(address(this), encryptionKey);
+        if (voteResult == SlmJudgement.VoteStates.MerchantWins) {
+            eligibleWithdrawal = true;
+        } else if (voteResult == SlmJudgement.VoteStates.Tie) {
+            isTie = true;
+        }
+        require(eligibleWithdrawal || isTie, "Cannot withdraw");
+
+        if (finalWithdrawal) {
+            state = TransactionState.CompleteTie;
+        } else {
+            state = TransactionState.CompleteParty2;
+        }
+
+        if (!merchantWithdrawalComplete) {  
+            withdraw(merchant(), owner, isTie, finalWithdrawal);
+        }
+        merchantWithdrawalComplete = true;
+        finalWithdrawal = true;
     }
 
 }
